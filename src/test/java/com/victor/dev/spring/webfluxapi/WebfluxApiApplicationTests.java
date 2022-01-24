71948f0dd0508a687a2ba1209b2e19b563ce575f
SelecGH <victormoares@gmail.com>
2022-01-24 18:12:58 +0100

    FEAT: Initial commmit

---
+++ src/test/java/com/victor/dev/spring/webfluxapi/WebfluxApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victor.dev.spring.webfluxapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
